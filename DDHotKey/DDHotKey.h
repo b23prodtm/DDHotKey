c4b7e2282bbcc2dc07c50a419380a46cd74a2956
Tina B Rakoto <webmaster@b23prodtm.info>
2025-03-02 18:29:59 +0100

    Shared Framework

---
+++ DDHotKey/DDHotKey.h
@@ -0,0 +1,19 @@
+//
+//  DDHotKey.h
+//  DDHotKey
+//
+//  Created by MacPro1 on 02/03/2025.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DDHotKey.
+FOUNDATION_EXPORT double DDHotKeyVersionNumber;
+
+//! Project version string for DDHotKey.
+FOUNDATION_EXPORT const unsigned char DDHotKeyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DDHotKey/PublicHeader.h>
+
+
